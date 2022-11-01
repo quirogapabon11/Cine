8768c1721fd625c354fc1d37f45b67edc803e051
Javier Merele <javiermereleet12d1@gmail.com>
2022-11-01 09:54:12 -0300

    UsuarioTests

---
+++ src/Tests/UsuarioTests.cs
@@ -0,0 +1,11 @@
+
+namespace Tests
+{
+    public class UsuarioTests
+    {
+        public void Agregar_CuandoSeCreaMiembro_RetornaVerdadero()
+        {
+            var miembro = new Miembro()
+        }
+    }
+}
