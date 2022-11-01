a5c32dab03a0d14d25192dd5b616873b1b3011d0
Esthefany Quiroga <quirogapabon11@gmail.com>
2022-11-01 09:53:02 -0300

    Test MiembroTests Agregado Correctamente

--- src/Cine/Entidades/Proyeccion.cs
+++ src/Cine/Entidades/Proyeccion.cs
@@ -6,14 +6,14 @@ namespace Cine.Entidades
         public int Sala { get; set; }
         public int Cine { get; set; }
         public string Formato { get; set; }
-        public DateTime Horarios { get; set; }
-        public Proyeccion(string pelicula, int sala, int cine, string formato, DateTime horarios)
+        public DateTime Horario { get; set; }
+        public Proyeccion(string pelicula, int sala, int cine, string formato, DateTime horario)
         {
             this.Pelicula = pelicula;
             this.Sala = sala;
             this.Cine = cine;
             this.Formato = formato;
-            this.Horarios = horarios;
+            this.Horario = horario;
         }
     }
 }
