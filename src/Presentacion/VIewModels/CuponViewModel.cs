ce85e616b0affd81029842bf8a4379cdab70d735
MAX <maximo.yampaet12d1@gmail.com>
2022-11-24 11:55:58 -0300

    controlador

--- src/Presentacion/VIewModels/CuponViewModel.cs
+++ src/Presentacion/VIewModels/CuponViewModel.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Presentacion.VIewModels
 {
     public class CuponViewModel
     {
         public int Descuento { get; set; }
+        public int Codigo { get; set; }
     }
 }
